c4fc2ef69d3810694e52b7ae0126efb7bc8b20a1
zsoltibaba37 <petozsolt33@gmail.com>
2022-09-24 13:05:55 +0200

    Delete display, add interrupt to button

--- RGB_Control.ino
+++ RGB_Control.ino
@@ -1,9 +1,10 @@
 /*
   RGB Control
   0.1 - three Option
+  0.2 - remove display, add attachInterrupt to button
 */
 
-float version = 0.1;
+float version = 0.2;
 
 // ------------------ LEDS ------------------
 // ------------------ LEDS ------------------
@@ -16,16 +17,16 @@ int BLUE_L = 9;
 int *leds[c] = { &RED_L, &GREEN_L, &BLUE_L };
 
 int LedState = 0;  // which state the first 0-3
-int ledSWState = LOW;
+int LedSWState = LOW;
 unsigned long prevMillis = 0;
 const int blinkDelay = 100;
-int ledSWState2 = LOW;
+int LedSWState2 = LOW;
 unsigned long prevMillis2 = 0;
 
 // ------------------ POTM ------------------
 // ------------------ POTM ------------------
 const int potR = A0;
-const int sNum = 2; // Sample number
+const int sNum = 10; // Sample number
 void smoothAnalog();
 uint32_t brightness;
 int lastBrightness = 0;
@@ -33,39 +34,43 @@ int lastBrightness = 0;
 // ------------------ BUTTON ------------------
 // ------------------ BUTTON ------------------
 int ButtonSW = 3;
-bool ButState;
 bool ButLastState;
+const int maxStates = 3;
 
 // ------------------ VOIDS ------------------
 // ------------------ VOIDS ------------------
 void sensButton();
+void debounceButton();
 void OnDisplay();
 void OffDisplay();
 void firstState();
 void secondState();
 void thirdState();
 void offState();
+void stateDisplay();
+
+bool once = true;
 
 // ------------------ I2C Oled ------------------
 // ------------------ I2C Oled ------------------
-#include <SPI.h>
-#include <Wire.h>
-#include <Adafruit_GFX.h>
-#include <Adafruit_SSD1306.h>
-
-#define SCREEN_WIDTH 128 // OLED display width, in pixels
-#define SCREEN_HEIGHT 64 // OLED display height, in pixels
-
-// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
-#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
-#define SCREEN_ADDRESS 0x3C
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+//#include <SPI.h>
+//#include <Wire.h>
+//#include <Adafruit_GFX.h>
+//#include <Adafruit_SSD1306.h>
+//
+//#define SCREEN_WIDTH 128 // OLED display width, in pixels
+//#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+//
+//// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+//#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
+//#define SCREEN_ADDRESS 0x3C
+//Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 // ------------------ SETUP ------------------
 // ------------------ SETUP ------------------
 void setup() {
-  Serial.begin(115200);
-  Serial.println("---RGB--Control---");
+  //  Serial.begin(115200);
+  //  Serial.println("---RGB--Control---");
 
   for (auto a : leds) {
     pinMode(*a, OUTPUT);
@@ -73,33 +78,32 @@ void setup() {
 
   pinMode(ButtonSW, INPUT_PULLUP);
   ButLastState = digitalRead(ButtonSW);
+  attachInterrupt(digitalPinToInterrupt(ButtonSW), sensButton, FALLING);
 
   // --------------- OLED ---------------
-  if (!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) { // Address 0x3D or 0x3C for 128x64
-    Serial.println(F("-- SSD1306 allocation failed --"));
-    for (;;);
-  }
+  //  if (!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) { // Address 0x3D or 0x3C for 128x64
+  //    Serial.println(F("-- SSD1306 allocation failed --"));
+  //    for (;;);
+  //  }
 
   // ------------------ INITIAL DISPLAY  ------------------
 
-  display.clearDisplay();
-  display.setTextSize(2);
-  display.setTextColor(WHITE);
-  display.setCursor(49, 10);
-  display.println("RGB");
-  display.setCursor(25, 30);
-  display.println("CONTROL");
-  display.display();
-  delay(10);
-  display.clearDisplay();
-
+  //  display.clearDisplay();
+  //  display.setTextSize(2);
+  //  display.setTextColor(WHITE);
+  //  display.setCursor(49, 10);
+  //  display.println("RGB");
+  //  display.setCursor(25, 30);
+  //  display.println("CONTROL");
+  //  display.display();
+  //  delay(10);
+  //  display.clearDisplay();
 }
 
 // ------------------ LOOP ------------------
 // ------------------ LOOP ------------------
 void loop() {
 
-  sensButton();
   if (LedState == 0) {
     offState();
   }
@@ -113,6 +117,8 @@ void loop() {
     thirdState();
   }
 
+  debounceButton();
+
 }
 
 // ------------------ VOIDS ------------------
@@ -121,10 +127,12 @@ void loop() {
 // ------------------ CASES ------------------
 
 void offState() {
-  OffDisplay();
-  for (auto a : leds) {
-    digitalWrite(*a, LOW);
-    delay(5);
+  while (once) {
+    for (auto a : leds) {
+      digitalWrite(*a, LOW);
+      delay(5);
+    }
+    once = false;
   }
 }
 
@@ -133,7 +141,7 @@ void firstState() {
   for (auto a : leds) {
     analogWrite(*a, brightness);
   }
-  OnDisplay();
+  delay(100);
 }
 
 void secondState() {
@@ -141,14 +149,13 @@ void secondState() {
     unsigned int currentMillis = millis();
     if (currentMillis - prevMillis >= blinkDelay) {
       prevMillis = currentMillis;
-      if (ledSWState == LOW) {
-        ledSWState = HIGH;
+      if (LedSWState == LOW) {
+        LedSWState = HIGH;
       }
       else {
-        ledSWState = LOW;
+        LedSWState = LOW;
       }
-      digitalWrite(*a, ledSWState);
-
+      digitalWrite(*a, LedSWState);
     }
   }
 }
@@ -158,13 +165,13 @@ void thirdState() {
     unsigned int currentMillis2 = millis();
     if (currentMillis2 - prevMillis2 >= int(analogRead(potR))) {
       prevMillis2 = currentMillis2;
-      if (ledSWState2 == LOW) {
-        ledSWState2 = HIGH;
+      if (LedSWState2 == LOW) {
+        LedSWState2 = HIGH;
       }
       else {
-        ledSWState2 = LOW;
+        LedSWState2 = LOW;
       }
-      digitalWrite(*a, ledSWState2);
+      digitalWrite(*a, LedSWState2);
     }
   }
 }
@@ -188,31 +195,42 @@ void smoothAnalog() {
 }
 
 // ------------------ Button ------------------
+// ----------------- Interrupt ----------------
 void sensButton() {
-  ButState = digitalRead(ButtonSW);
-  if ( ButLastState == true && ButState == false) {
+  if ( ButLastState == true ) {
     LedState++;
+    once = true;
     ButLastState = false;
-    if ( LedState > 3 ) {
+    if ( LedState > maxStates ) {
       LedState = 0;
     }
   }
-  if (ButState == HIGH) {
+}
+
+void debounceButton() {
+  if (digitalRead(ButtonSW)) {
     ButLastState = true;
   }
 }
 
 // ------------------ Displays ------------------
-void OnDisplay() {
-  display.clearDisplay();
-  display.setCursor(1, 25);
-  display.print("Bright:" + (String)brightness);
-  display.display();
-}
-
-void OffDisplay() {
-  display.clearDisplay();
-  display.setCursor(1, 25);
-  display.println("Press btn.");
-  display.display();
-}
+//void OnDisplay() {
+//  display.clearDisplay();
+//  display.setCursor(1, 25);
+//  display.print("Bright:" + (String)brightness);
+//  display.display();
+//}
+//
+//void OffDisplay() {
+//  display.clearDisplay();
+//  display.setCursor(1, 25);
+//  display.println("Press btn.");
+//  display.display();
+//}
+//
+//void stateDisplay() {
+//  display.clearDisplay();
+//  display.setCursor(10, 25);
+//  display.print((String)LedState);
+//  display.display();
+//}
